09c258212c364c563c3a5aaf5106a69e692b0a12
GToidZ <39559733+GToidZ@users.noreply.github.com>
2023-03-14 10:17:29 +0700

    feat: add GET all todos and POST todo routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import todoRouter from './routes/todo.js';
 
 const app = express();
+
+app.all("/", (_, res) => {
+    res.send("Welcome to GDSC!");
+});
+
+/*
+ *  GET /todo
+ *  Pass on to Todo router
+ */
+app.use("/todo", todoRouter);
 
 app.listen(8080, () => {
     console.log("Server started at port 8080");
---
+++ backend/routes/todo.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dayjs from 'dayjs';
+
+const router = express.Router();
+const jsonParser = bodyParser.json({ extended: false });
+
+const toFormatDateTime = (dateObject) => {
+    return dayjs(dateObject).format("DD/MM/YYYY HH:mm");
+};
+
+/*
+ *  GET /
+ *  Returns all todo entries
+ */
+router.get("/", (_, res) => {
+    res.send("TODO: GET / All todo entries");
+});
+
+/*
+ *  POST /create
+ *  Takes a form data (name, description) and creates a new
+ *  todo entry in Redis.
+ */
+router.post("/create", jsonParser, (req, res) => {
+    const todoName = req.body.name;
+
+    if (!todoName) {
+        res.status(400).json({
+            error: "You must specify a name for Todo."
+        });
+    }
+
+    const todoDescription = req.body.description || "";
+    const todoCreatedAt = Date.now();
+    
+    // TODO: Save the Todo object into Redis database.
+
+    res.json({
+        // TODO: Add returning todo ID, and status.
+        name: todoName,
+        description: todoDescription,
+        createdAt: toFormatDateTime(todoCreatedAt),
+    })
+});
+
+export default router;
